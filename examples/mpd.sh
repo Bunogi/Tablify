8281289b2586f50eb3fc63382757415b88e9c8c3
Norr <norr@norrfire.tk>
2015-04-18 16:33:58 +0200

    Added examples

---
+++ examples/mpd.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+#Select a new song from your mpd playlist. Reqires mpd and mpc.
+#To be run inside the examples directory.
+
+changeto=$(mpc --format "%position%\t%artist%\t%title%" playlist | actualMPD -r 0 Number Artist Title)
+
+if [ ! -z "$changeto" ]
+then
+	mpc play $changeto
+fi
