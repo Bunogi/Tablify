3b492151d7697f3e17a33e5f3f1d6113a11736bf
Norr <norr@norrfire.tk>
2015-06-09 21:35:34 +0200

    Version 1.0! Yay!

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+//Tablify
+//Written by Bunogi <bunogi@cwan.nl>
+
 //Std includes
 #include <string>
 #include <vector>
@@ -11,14 +14,14 @@
 #include <QtGui>
 
 //These need to be global
-TCLAP::CmdLine cmd("Put formatted text into a browseable, searchable table.", ' ', "0.9-master");
+TCLAP::CmdLine cmd("Put formatted text into a browseable, searchable table.", ' ', "1.0-master");
 TCLAP::ValueArg<int> returnArg("r", "returncolumn", "Set the column to return data from. The output of this program will be the value inside ( <selected row>, <value of -r> ) ", false, 0, "column");
 TCLAP::ValueArg<std::string> formatArg("f", "format", "The format character. This is where the input is split. Defaults to tab(\\t)", false, "\t", "format");
 TCLAP::ValueArg<int> minWidthArg("W", "width", "Starting width of the window.", false, 800, "width");
 TCLAP::ValueArg<int> minHeightArg("H", "height", "Starting height of the window.", false, 300, "height");
 TCLAP::ValueArg<int> hideColArg("c", "hidecol", "Hide column n", false, -1, "Column to hide");
 TCLAP::SwitchArg closeArg("n", "noclose", "Do not close program after selecting");
-TCLAP::UnlabeledMultiArg<std::string> colArg("names", "The names for the collumns, each name you type in will create a new column.",  true, "column names");
+TCLAP::UnlabeledMultiArg<std::string> colArg("names", "The names for the collumns, each name you type in will create a new column.",  false, "column names");
 
 int main(int argc, char *argv[])
 {
